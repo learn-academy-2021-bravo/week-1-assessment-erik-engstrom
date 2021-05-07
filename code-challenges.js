830a435ce6e979e3484fe969beaf5b97878e2bb7
erik-engstrom <eekareeka@yahoo.com>
2021-05-07 16:31:29 -0700

    challenges complete thru #4

--- code-challenges.js
+++ code-challenges.js
@@ -9,9 +9,39 @@
 // --------------------1) Create a function that takes in a number and determines if a temperature is below boiling point, at boiling point or above boiling point. Boiling point is 212 degrees Fahrenheit.
 // Use the test variables provided below. Expected output: "35 is below boiling point", "350 is above boiling point", "212 is at boiling point"
 
-var temp1 = 35
-var temp2 = 350
-var temp3 = 212
+// var temp1 = 35
+// var temp2 = 350
+// var temp3 = 212
+
+// // Write a function that takes one parameter 
+// const boiling = (temp) => {
+//     // Create an empty "basket" to add the function results to what will be returned
+//     let isItBoiling = ("")
+//     // Need an if statement to look for if the temperature is below boiling point
+//     if (temp < 212){
+//         console.log(`${temp} is below boiling point`);
+//     }
+//     // Need an if statement to look for if the temperature is at boiling point
+//     else if(temp == 212) {
+//         console.log(`${temp} is at boiling point`);
+//     }
+//     // Need an if statement to look for if the temperature is above boiling point
+//     else if(temp > 212){
+//         console.log(`${temp} is above boiling point`);
+//     }
+//     // Catch all in case a number isn't passed through as an argument
+//     else{
+//         "Something messed up somewhere, try entering a number"
+//     }
+//     // Return the "basket" and it's new contents generated from the function
+//     return isItBoiling
+// }
+// // Invoke the function and pass in the argument(s) to be evaluated.
+// console.log(boiling(temp1));
+// console.log(boiling(temp2));
+// console.log(boiling(temp3));
+
+
 
 
 
@@ -20,7 +50,21 @@ var temp3 = 212
 // --------------------2) Create a function that takes in an array of numbers and returns an array with each number multiplied by 5.
 // Use the test variable provided below. Expected output: [15, 35, 0, 30, -45]
 
-var myNumbers1 = [3, 7, 0, 6, -9]
+// var myNumbers1 = [3, 7, 0, 6, -9]
+ // Write a function that takes one parameter 
+// const multiply = (number) => {
+     // Set a new empty "basket" to collect the function evaluations
+//     let newNumbers = [];
+     // for loop to iterate over the array
+//     for(i=0; i<number.length; i++){
+       // Action to be taken for each number in the array
+//         newNumbers.push(number[i]*5)
+//     } 
+     // Return the new array with the multiplied values
+//     return newNumbers
+// }
+ // Pass in the argument to the multiply function to be evaluated
+// console.log(multiply(myNumbers1));
 
 
 
@@ -29,7 +73,24 @@ var myNumbers1 = [3, 7, 0, 6, -9]
 // --------------------3) Create a function that takes in an array of numbers and returns an array containing only the odd numbers.
 // Use the test variable provided below. Expected output: [-7, 9, 13]
 
-var myNumbers2 = [8, -7, 0, 6, 2, 9, 13]
+// var myNumbers2 = [8, -7, 0, 6, 2, 9, 13]
+// // Write a function that takes one parameter 
+// const giveOdds = (nums) =>{ 
+//     // define an empty array to return the results
+//     let oddNums = []
+//     // iterate over the array 
+//     for ( let i=0; i<nums.length; i++){
+//         // need an if statement to reject even numbers and return odd numbers
+//         if(nums[i] % 2 !==0){
+//             // action to be taken with odd numbers. Add to new array oddNums
+//             oddNums.push(nums[i])
+//         }
+//     }
+//     // Return the new contents of the evaluated function
+//     return oddNums
+// }
+// // Invoke the function giveOdds
+// console.log(giveOdds(myNumbers2))
 
 
 
@@ -38,9 +99,17 @@ var myNumbers2 = [8, -7, 0, 6, 2, 9, 13]
 // --------------------4) Create a function that removes all the vowels from a string.
 // Use the test variables provided below. Expected output: "HyThrLrnStdnt" "LvJvScrpt"
 
-var stringWithVowels1 = "HeyThereLearnStudent"
-var stringWithVowels2 = "ILoveJavaScript"
-
+// var stringWithVowels1 = "HeyThereLearnStudent"
+// var stringWithVowels2 = "ILoveJavaScript"
+// // Write a function that takes one parameter 
+// const noVowels = (string) => {
+//     // define empty string to return
+    
+//     return string.replace(/[aeiou]/g, '')
+    
+// }
+// console.log(noVowels(stringWithVowels1))
+// console.log(noVowels(stringWithVowels2))
 
 
 
--- interview-questions.md
+++ interview-questions.md
@@ -7,47 +7,50 @@ Then, research the question to expand on your answer. Even if you feel you have 
 
 1. What is a function? Why would you use one?
 
-  Your answer:
+  Your answer: A function is an algorithm that takes a particular input and results an output based on what you instruct the function to return. I would use a function where multiple outputs needed to be returned and don't want to evaluate each one seperately. Functions are reusable so the logic of the function would only needed to be written once and multiple arguments can be passed into the function and returned.    
 
-  Researched answer:
+  Researched answer: "A function in JavaScript is similar to a procedure—a set of statements that performs a task or calculates a value, but for a procedure to qualify as a function, it should take some input and return an output where there is some obvious relationship between the input and the output." -MDN
 
 
 
 2. What is the difference between console.log() and return?
 
-  Your answer:
+  Your answer: console.log() will show the value, in your terminal or console, of what you're asking to return. Return does not print out the value but the value is stored in the logic of the function.  
 
-  Researched answer:
+  Researched answer: "So, console.log is a function that logs the arguments you pass to the web console. return is a statement which specifies the value returned from a function." -Reddit
 
 
 
 3. In regards to functions, what is the difference between an argument and a parameter?
 
-  Your answer:
+  Your answer: The argument is what gets passed into the function to be evalauated. The parameter is kind of like the placeholder defined as something that will be coming later.
 
-  Researched answer:
+  Researched answer: 
+"- Function parameters are the names listed in the function's definition.
+- Function arguments are the real values passed to the function.
+- Parameters are initialized to the values of the arguments supplied." -MDN
 
 
 
 4. What is a built-in method?
 
-  Your answer:
+  Your answer: A built in method is like a shortcut of code logic programmed into the language by the language creator(s).
 
-  Researched answer:
+  Researched answer: "In JavaScript, built-in functions are the global functions that are called globally, rather than on an object." -Programiz.com
 
 
 
 5. Give a brief description of proper pair programming techniques. What are the roles of each person?
 
-  Your answer:
+  Your answer: Proper pair programming consists of one programmer programming on the computer AKA "driving" and the 2nd programmer is "navigating" what the driver should be doing. THe navigator is also responsible for looking out for syntax errors and proper syntax spacing/formatting. The driver will be inputting the code to the text editor and the navigator will be researching topics and informing the driver of what should be coming next.
 
-  Researched answer:
+  Researched answer: "Pair programming is basically a growing software development technique in which two individuals work on the single block of code. The programmers undertake one of two parts — the Driver and the navigator. The driver in the process is responsible for diligently drafting code, while the navigator’s job is to review and concentrates on the plan of action." - Medium
 
 
 
 6. What is something we did in class this week you found helpful?  
 
-  Your answer:
+  Your answer: 
 
 
 
